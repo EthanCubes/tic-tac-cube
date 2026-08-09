2e1bbba3bd1bb7de2ecc9bef4864be6656c40c2b
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-09 16:02:12 -0600

    Fixed typo where the code was missing a colon

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "board.h"
 
 int main() {
-    std::array<std::array<std:array<int, 3>, 3>, 6> cube_pos = get_board();
+    std::array<std::array<std::array<int, 3>, 3>, 6> cube_pos = get_board();
     std::cout << cube_pos;
     return 0;
 }
