b142b733f4834af0042ffdc8bb4aff9953eda6e1
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-09 15:40:31 -0600

    Added code to print out the board position from board.cpp. Testing in case compiling overrides my code again

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <array>
+#include "board.h"
 
 int main() {
+    std::array<std::array<std:array<int, 3>, 3>, 6> cube_pos = get_board();
+    std::cout << cube_pos;
     return 0;
 }
