d5c99b040a2f6c3a8e1ef9a6e07ea5621a9f7306
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-09 13:06:22 -0600

    Added file to store and change the position of the board.

---
+++ src/board.cpp
@@ -0,0 +1,13 @@
+// 0 is white, 1 is orange, 2 is green, 3 is red, 4 is blue, 5 is yellow
+string faces[6][9] = {
+    {0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1},
+    {2, 2, 2, 2, 2, 2},
+    {3, 3, 3, 3, 3, 3},
+    {4, 4, 4, 4, 4, 4},
+    {5, 5, 5, 5, 5, 5}
+};
+
+int main() {
+    return 0;
+};
