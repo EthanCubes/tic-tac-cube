0acd6333551365112610b9ad13602107fb2eab9f
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-09 16:23:31 -0600

    Fixed character out statement trying to output an entire three dimensional array by printing each element of the array individually

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@
 
 int main() {
     std::array<std::array<std::array<int, 3>, 3>, 6> cube_pos = get_board();
-    std::cout << cube_pos;
+    for (int face = 0; face < 6; face++) {
+        for (int row = 0; row < 3; row++) {
+            for (int column = 0; column < 3; column++) {
+                std::cout << cube_pos[face][row][column];
+            }
+            std::cout << "\n";
+        }
+        std::cout << "\n";
+    }
     return 0;
 }
