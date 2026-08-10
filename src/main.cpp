d58aea1b57f61de2dd467f8c8252443bdd895c77
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-09 20:58:00 -0600

    Added basic movement of cube, which I am calculating before the actual tic-tac-toe part

--- src/main.cpp
+++ src/main.cpp
@@ -43,15 +43,258 @@ class Cube_board {
         
         void x_move() {
             temporary_array = positions;
+            // White
+            positions[0][0][0] = temporary_array[2][0][0];
+            positions[0][0][1] = temporary_array[2][0][1];
+            positions[0][0][2] = temporary_array[2][0][2];
+            positions[0][1][0] = temporary_array[2][1][0];
+            positions[0][1][1] = temporary_array[2][1][1];
+            positions[0][1][2] = temporary_array[2][1][2];
+            positions[0][2][0] = temporary_array[2][2][0];
+            positions[0][2][1] = temporary_array[2][2][1];
+            positions[0][2][2] = temporary_array[2][2][2];
+            // Orange
+            positions[1][0][0] = temporary_array[1][0][2];
+            positions[1][0][1] = temporary_array[1][1][2];
+            positions[1][0][2] = temporary_array[1][2][2];
+            positions[1][1][0] = temporary_array[1][0][1];
+            positions[1][1][1] = temporary_array[1][1][1];
+            positions[1][1][2] = temporary_array[1][2][1];
+            positions[1][2][0] = temporary_array[1][0][0];
+            positions[1][2][1] = temporary_array[1][1][0];
+            positions[1][2][2] = temporary_array[1][2][0];
+            // Green
+            positions[2][0][0] = temporary_array[5][0][0];
+            positions[2][0][1] = temporary_array[5][0][1];
+            positions[2][0][2] = temporary_array[5][0][2];
+            positions[2][1][0] = temporary_array[5][1][0];
+            positions[2][1][1] = temporary_array[5][1][1];
+            positions[2][1][2] = temporary_array[5][1][2];
+            positions[2][2][0] = temporary_array[5][2][0];
+            positions[2][2][1] = temporary_array[5][2][1];
+            positions[2][2][2] = temporary_array[5][2][2];
+            // Red
+            positions[3][0][0] = temporary_array[3][2][0];
+            positions[3][0][1] = temporary_array[3][1][0];
+            positions[3][0][2] = temporary_array[3][0][0];
+            positions[3][1][0] = temporary_array[3][2][1];
+            positions[3][1][1] = temporary_array[3][1][1];
+            positions[3][1][2] = temporary_array[3][0][1];
+            positions[3][2][0] = temporary_array[3][2][2];
+            positions[3][2][1] = temporary_array[3][1][2];
+            positions[3][2][2] = temporary_array[3][0][2];
+            // Blue
+            positions[4][0][0] = temporary_array[0][2][2];
+            positions[4][0][1] = temporary_array[0][2][1];
+            positions[4][0][2] = temporary_array[0][2][0];
+            positions[4][1][0] = temporary_array[0][1][2];
+            positions[4][1][1] = temporary_array[0][1][1];
+            positions[4][1][2] = temporary_array[0][1][0];
+            positions[4][2][0] = temporary_array[0][0][2];
+            positions[4][2][1] = temporary_array[0][0][1];
+            positions[4][2][2] = temporary_array[0][0][0];
+            // Yellow
+            positions[5][0][0] = temporary_array[4][2][2];
+            positions[5][0][1] = temporary_array[4][2][1];
+            positions[5][0][2] = temporary_array[4][2][0];
+            positions[5][1][0] = temporary_array[4][1][2];
+            positions[5][1][1] = temporary_array[4][1][1];
+            positions[5][1][2] = temporary_array[4][1][0];
+            positions[5][2][0] = temporary_array[4][0][2];
+            positions[5][2][1] = temporary_array[4][0][1];
+            positions[5][2][2] = temporary_array[4][0][0];
         };
+
         void y_move() {
             temporary_array = positions;
+            // White
+            positions[0][0][0] = temporary_array[0][2][0];
+            positions[0][0][1] = temporary_array[0][1][0];
+            positions[0][0][2] = temporary_array[0][0][0];
+            positions[0][1][0] = temporary_array[0][2][1];
+            positions[0][1][1] = temporary_array[0][1][1];
+            positions[0][1][2] = temporary_array[0][0][1];
+            positions[0][2][0] = temporary_array[0][2][2];
+            positions[0][2][1] = temporary_array[0][1][2];
+            positions[0][2][2] = temporary_array[0][0][2];
+            // Orange
+            positions[1][0][0] = temporary_array[2][0][0];
+            positions[1][0][1] = temporary_array[2][0][1];
+            positions[1][0][2] = temporary_array[2][0][2];
+            positions[1][1][0] = temporary_array[2][1][0];
+            positions[1][1][1] = temporary_array[2][1][1];
+            positions[1][1][2] = temporary_array[2][1][2];
+            positions[1][2][0] = temporary_array[2][2][0];
+            positions[1][2][1] = temporary_array[2][2][1];
+            positions[1][2][2] = temporary_array[2][2][2];
+            // Green
+            positions[2][0][0] = temporary_array[3][0][0];
+            positions[2][0][1] = temporary_array[3][0][1];
+            positions[2][0][2] = temporary_array[3][0][2];
+            positions[2][1][0] = temporary_array[3][1][0];
+            positions[2][1][1] = temporary_array[3][1][1];
+            positions[2][1][2] = temporary_array[3][1][2];
+            positions[2][2][0] = temporary_array[3][2][0];
+            positions[2][2][1] = temporary_array[3][2][1];
+            positions[2][2][2] = temporary_array[3][2][2];
+            // Red
+            positions[3][0][0] = temporary_array[4][0][0];
+            positions[3][0][1] = temporary_array[4][0][1];
+            positions[3][0][2] = temporary_array[4][0][2];
+            positions[3][1][0] = temporary_array[4][1][0];
+            positions[3][1][1] = temporary_array[4][1][1];
+            positions[3][1][2] = temporary_array[4][1][2];
+            positions[3][2][0] = temporary_array[4][2][0];
+            positions[3][2][1] = temporary_array[4][2][1];
+            positions[3][2][2] = temporary_array[4][2][2];
+            // Blue
+            positions[4][0][0] = temporary_array[1][0][0];
+            positions[4][0][1] = temporary_array[1][0][1];
+            positions[4][0][2] = temporary_array[1][0][2];
+            positions[4][1][0] = temporary_array[1][1][0];
+            positions[4][1][1] = temporary_array[1][1][1];
+            positions[4][1][2] = temporary_array[1][1][2];
+            positions[4][2][0] = temporary_array[1][2][0];
+            positions[4][2][1] = temporary_array[1][2][1];
+            positions[4][2][2] = temporary_array[1][2][2];
+            // Yellow
+            positions[5][0][0] = temporary_array[5][0][2];
+            positions[5][0][1] = temporary_array[5][1][2];
+            positions[5][0][2] = temporary_array[5][2][2];
+            positions[5][1][0] = temporary_array[5][0][1];
+            positions[5][1][1] = temporary_array[5][1][1];
+            positions[5][1][2] = temporary_array[5][2][1];
+            positions[5][2][0] = temporary_array[5][0][0];
+            positions[5][2][1] = temporary_array[5][1][0];
+            positions[5][2][2] = temporary_array[5][2][0];
         };
+
         void z_move() {
             temporary_array = positions;
+            // White
+            positions[0][0][0] = temporary_array[1][2][0];
+            positions[0][0][1] = temporary_array[1][1][0];
+            positions[0][0][2] = temporary_array[1][0][0];
+            positions[0][1][0] = temporary_array[1][2][1];
+            positions[0][1][1] = temporary_array[1][1][1];
+            positions[0][1][2] = temporary_array[1][0][1];
+            positions[0][2][0] = temporary_array[1][2][2];
+            positions[0][2][1] = temporary_array[1][1][2];
+            positions[0][2][2] = temporary_array[1][0][2];
+            // Orange
+            positions[1][0][0] = temporary_array[5][2][0];
+            positions[1][0][1] = temporary_array[5][1][0];
+            positions[1][0][2] = temporary_array[5][0][0];
+            positions[1][1][0] = temporary_array[5][2][1];
+            positions[1][1][1] = temporary_array[5][1][1];
+            positions[1][1][2] = temporary_array[5][0][1];
+            positions[1][2][0] = temporary_array[5][2][2];
+            positions[1][2][1] = temporary_array[5][1][2];
+            positions[1][2][2] = temporary_array[5][0][2];
+            // Green
+            positions[2][0][0] = temporary_array[2][2][0];
+            positions[2][0][1] = temporary_array[2][1][0];
+            positions[2][0][2] = temporary_array[2][0][0];
+            positions[2][1][0] = temporary_array[2][2][1];
+            positions[2][1][1] = temporary_array[2][1][1];
+            positions[2][1][2] = temporary_array[2][0][1];
+            positions[2][2][0] = temporary_array[2][2][2];
+            positions[2][2][1] = temporary_array[2][1][2];
+            positions[2][2][2] = temporary_array[2][0][2];
+            // Red
+            positions[3][0][0] = temporary_array[0][2][0];
+            positions[3][0][1] = temporary_array[0][1][0];
+            positions[3][0][2] = temporary_array[0][0][0];
+            positions[3][1][0] = temporary_array[0][2][1];
+            positions[3][1][1] = temporary_array[0][1][1];
+            positions[3][1][2] = temporary_array[0][0][1];
+            positions[3][2][0] = temporary_array[0][2][2];
+            positions[3][2][1] = temporary_array[0][1][2];
+            positions[3][2][2] = temporary_array[0][0][2];
+            // Blue
+            positions[4][0][0] = temporary_array[4][2][0];
+            positions[4][0][1] = temporary_array[4][1][0];
+            positions[4][0][2] = temporary_array[4][0][0];
+            positions[4][1][0] = temporary_array[4][2][1];
+            positions[4][1][1] = temporary_array[4][1][1];
+            positions[4][1][2] = temporary_array[4][0][1];
+            positions[4][2][0] = temporary_array[4][2][2];
+            positions[4][2][1] = temporary_array[4][1][2];
+            positions[4][2][2] = temporary_array[4][0][2];
+            // Yellow
+            positions[5][0][0] = temporary_array[3][0][0];
+            positions[5][0][1] = temporary_array[3][0][1];
+            positions[5][0][2] = temporary_array[3][0][2];
+            positions[5][1][0] = temporary_array[3][1][0];
+            positions[5][1][1] = temporary_array[3][1][1];
+            positions[5][1][2] = temporary_array[3][1][2];
+            positions[5][2][0] = temporary_array[3][2][0];
+            positions[5][2][1] = temporary_array[3][2][1];
+            positions[5][2][2] = temporary_array[3][2][2];
         };
+
         void u_move() {
             temporary_array = positions;
+            // White
+            positions[0][0][0] = temporary_array[0][2][0];
+            positions[0][0][1] = temporary_array[0][1][0];
+            positions[0][0][2] = temporary_array[0][0][0];
+            positions[0][1][0] = temporary_array[0][2][1];
+            positions[0][1][1] = temporary_array[0][1][1];
+            positions[0][1][2] = temporary_array[0][0][1];
+            positions[0][2][0] = temporary_array[0][2][2];
+            positions[0][2][1] = temporary_array[0][1][2];
+            positions[0][2][2] = temporary_array[0][0][2];
+            // Orange
+            positions[1][0][0] = temporary_array[2][0][0];
+            positions[1][0][1] = temporary_array[2][0][1];
+            positions[1][0][2] = temporary_array[2][0][2];
+            positions[1][1][0] = temporary_array[1][1][0];
+            positions[1][1][1] = temporary_array[1][1][1];
+            positions[1][1][2] = temporary_array[1][1][2];
+            positions[1][2][0] = temporary_array[1][2][0];
+            positions[1][2][1] = temporary_array[1][2][1];
+            positions[1][2][2] = temporary_array[1][2][2];
+            // Green
+            positions[2][0][0] = temporary_array[3][0][0];
+            positions[2][0][1] = temporary_array[3][0][1];
+            positions[2][0][2] = temporary_array[3][0][2];
+            positions[2][1][0] = temporary_array[2][1][0];
+            positions[2][1][1] = temporary_array[2][1][1];
+            positions[2][1][2] = temporary_array[2][1][2];
+            positions[2][2][0] = temporary_array[2][2][0];
+            positions[2][2][1] = temporary_array[2][2][1];
+            positions[2][2][2] = temporary_array[2][2][2];
+            // Red
+            positions[3][0][0] = temporary_array[4][0][0];
+            positions[3][0][1] = temporary_array[4][0][1];
+            positions[3][0][2] = temporary_array[4][0][2];
+            positions[3][1][0] = temporary_array[3][1][0];
+            positions[3][1][1] = temporary_array[3][1][1];
+            positions[3][1][2] = temporary_array[3][1][2];
+            positions[3][2][0] = temporary_array[3][2][0];
+            positions[3][2][1] = temporary_array[3][2][1];
+            positions[3][2][2] = temporary_array[3][2][2];
+            // Blue
+            positions[4][0][0] = temporary_array[1][0][0];
+            positions[4][0][1] = temporary_array[1][0][1];
+            positions[4][0][2] = temporary_array[1][0][2];
+            positions[4][1][0] = temporary_array[4][1][0];
+            positions[4][1][1] = temporary_array[4][1][1];
+            positions[4][1][2] = temporary_array[4][1][2];
+            positions[4][2][0] = temporary_array[4][2][0];
+            positions[4][2][1] = temporary_array[4][2][1];
+            positions[4][2][2] = temporary_array[4][2][2];
+            // Yellow
+            positions[5][0][0] = temporary_array[5][0][0];
+            positions[5][0][1] = temporary_array[5][0][1];
+            positions[5][0][2] = temporary_array[5][0][2];
+            positions[5][1][0] = temporary_array[5][1][0];
+            positions[5][1][1] = temporary_array[5][1][1];
+            positions[5][1][2] = temporary_array[5][1][2];
+            positions[5][2][0] = temporary_array[5][2][0];
+            positions[5][2][1] = temporary_array[5][2][1];
+            positions[5][2][2] = temporary_array[5][2][2];
         };
         // These are are the moved made out of the basic moves
 };
