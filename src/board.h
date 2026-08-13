7a8539679a0253096a83ac18d163df9bb0954e55
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-13 00:04:15 -0600

    Added header file for the board.cpp file

---
+++ src/board.h
@@ -0,0 +1,80 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <iostream>
+#include <array>
+#include <string>
+#include <tuple>
+
+class Cube_board {
+    public:
+        Cube_board() {}
+        std::array<std::array<std::array<int, 3>, 3>, 6> positions = {{
+            {{
+                {0, 0, 0},
+                {0, 0, 0}, 
+                {0, 0, 0} 
+            }},
+            {{
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+            }},
+            {{
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+            }},
+            {{
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+            }},
+            {{
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+            }},
+            {{
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+            }}
+        }}; 
+        std::array<std::array<std::array<int, 3>, 3>, 6> temporary_array;
+        void x_move();
+        void y_move();
+        void z_move();
+        void u_move();
+        int turn = 1;
+        void x_prime();
+        void y_prime();
+        void z_prime();
+        void u_prime();
+        void l_move();
+        void l_prime();
+        void f_move();
+        void f_prime();
+        void r_move();
+        void r_prime();
+        void b_move();
+        void b_prime();
+        void d_move();
+        void d_prime();
+        void m_move();
+        void m_prime();
+        void e_move();
+        void e_prime();
+        void s_move();
+        void s_prime();
+        int mark_x(int x, int y);
+        int mark_o(int x, int y);
+        void print_full_board();
+        void print_current_side();
+        void move_cube(std::string move);
+        std::tuple<int, std::array<std::array<int, 2>, 3>> scan_for_wins();
+        void render_board();
+        void gameloop();
+};
+
+#endif
