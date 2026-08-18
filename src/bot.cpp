6b8278ac4308c715ca65c1509829b3fe02f25bb0
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-18 12:25:16 -0600

    Added function to scan for unbroken two pattern

--- src/bot.cpp
+++ src/bot.cpp
@@ -16,6 +16,67 @@ std::array<std::array<int, 3>, 3> convert_board_position(std::array<std::array<s
     return converted_board_position;
 };
 
+std::string scan_unbroken_two(std::array<int, 3>, 3> board_position) {
+    // for every place in the square, given that it is still inside the board
+    std::array<std::string, 8> direction_keys_array = {
+        "N",
+        "NE",
+        "E",
+        "SE",
+        "S",
+        "SW",
+        "W",
+        "NW"
+    };
+    std::map<std::string, std::array<int, 2> direction_modifier_index = {
+        {"N", {0, 1}},
+        {"NE", {1, 1}},
+        {"E", {1, 0}}, 
+        {"SE", {1, -1}},
+        {"S", {0, -1}},
+        {"SW", {-1, -1}},
+        {"W", {-1, 0}},
+        {"NW", {-1, 1}}
+    };
+    std::map<int, std::array<int, 2>> location_index = {
+        {{0, 0}, "p1"},
+        {{0, 1}, "p2"},
+        {{0, 2}, "p3"},
+        {{1, 0}, "p4"},
+        {{1, 1}, "p5"},
+        {{1, 2}, "p6"},
+        {{2, 0}, "p7"},
+        {{2, 1}, "p8"},
+        {{2, 2}, "p9"}
+    };
+
+    std::array<std::array<int, 2>, 3> scanned_positions;
+    for (int row = 0; row < 3; row++) {
+        for (int column = 0; column < 3; column++) {
+            for (int direction = 0; direction < 8; direction++) {
+                std::string direction_key = direction_keys_array[direction];
+                std::array<int, 2> direction_modifer = direction_modifier_index[direction_key];
+                int direction_x_modifier = direction_modifier[0];
+                int direction_y_modifier = direction_modifier[1];
+                scanned_positions[0] = {row, column};
+                scanned_positions[1] = {row + direction_x_modifier, column + direction_x_modifier};
+                scanned_positions[2] = {row + direction_x_modifier*2, column + direction_y_modifier*2};
+                if (scanned_positions[2][0] < 0 || scanned_positions[2][0] > 2 || scanned_positions[2][1] < 0 || scanned_positions[2][1] > 2) {
+                    continue;
+                }
+                else if (board_position[scanned_positions[0][0]][scanned_positions[0][1]] != 0) {
+                    continue;
+                }
+                // Actually scanning the thing
+                if (board_position[scanned_positions[1][0]][scanned_positions[1][1]] == board_position[scanned_positions[2][0]][scanned_positions[2][1]]) {
+                    return location_index[scanned_positions[0][0]][scanned_positions[0][1]];
+                }
+            }
+        }
+    }
+    return "nothing";
+}
+
 // These functions will scan for patterns
 std::string place_randomly(std::array<std::array<int, 3>, 3> board_position) {
     // Didn't actually test by compiling, lowkey asked DeepSeek if I made any mistakes.
@@ -93,7 +154,10 @@ std::string get_bot_move(std::array<std::array<std::array<int, 3>, 3>, 6> board_
     // Not technically needed, but the original array contains a bunch of redundant variables for this program.
     std::array<std::array<int, 3>, 3> converted_board_position = convert_board_position(board_position_input);
     
+    move = scan_unbroken_two(converted_board_position);
+    if (move != "nothing") {
+        return move;
+    }
     move = place_randomly(converted_board_position);
-
     return move;
 };
