5018ca085577c5855c90ca879459f1ca074b04e2
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-18 19:53:43 -0600

    Added some functions for making good moves as the bot

--- src/bot.cpp
+++ src/bot.cpp
@@ -17,12 +17,17 @@ std::array<std::array<int, 3>, 3> convert_board_position(std::array<std::array<s
     return converted_board_position;
 };
 
-std::string achieve_win(std::array<std::array<int, 3>, 3> board_position) {};
-std::string combat_formed_fork(std::array<std::array<int, 3>, 3> board_position) {};
-std::string block_win(std::array<std::array<int, 3>, 3> board_positon) {};
-std::string create_fork(std::array<std::array<int, 3>, 3> board_position) {};
-std::string block_fork_creation(std::array<std::array<int, 3>, 3> board_position) {};
-std::string place_priority(std::array<std::array<int, 3>, 3> board_position) {
+int convert_coords(std::array<int, 2> coords) {
+    int converted = coords[0] * 3 + coords[1] + 1;
+    return converted;
+};
+
+std::string achieve_win(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {};
+std::string combat_formed_fork(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {};
+std::string block_win(std::array<std::array<int, 3>, 3> board_positon, int bot_turn) {};
+std::string create_fork(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {};
+std::string block_fork_creation(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {};
+std::string place_priority(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {
     srand(time(NULL));
     // This function will be split into several parts: center, opposite corner, empty corner, empty side, and rotate board for new face
     // Center
@@ -30,8 +35,43 @@ std::string place_priority(std::array<std::array<int, 3>, 3> board_position) {
         return "p5";
     };
     // Opposite corner
+    /*
+     * Steps:
+     * Scan to board for an occupied corner by the enemy
+     * Place on opposite corner
+     */
     // Empty corner
+    std::array<std::array<int, 2>, 4> board_corners = {
+        {0, 0},
+        {0, 2},
+        {2, 0},
+        {2, 2}
+    };
+    for (int corners = 0; corners < 4; corners++) {
+        std::array<int, 2> current_side = {
+            board_sides[corners][0],
+            board_sides[corners][1]
+        };
+        if (board_position[current_side[0]][current_side[1]] == 0) {
+            return convert_coords(current_side);
+        };
+    };
     // Empty side
+    std::array<std::array<int, 2>, 4> board_sides = {
+        {1, 0},
+        {0, 1},
+        {2, 1},
+        {1, 2}
+    };
+    for (int sides = 0; sides < 4; sides++) {
+        std::array<int, 2> current_side = {
+            board_sides[sides][0],
+            board_sides[sides][1]
+        };
+        if (board_position[current_side[0]][current_side[1]] == 0) {
+            return convert_coords(current_side);
+        };
+    };
     // Rotate board
     rotation_type_number = rand() % 4;
     switch(rotation_type_number) {
@@ -45,12 +85,12 @@ std::string place_priority(std::array<std::array<int, 3>, 3> board_position) {
             return "mXp";
         default:
             return "mD";
-    }
+    };
 };
 
 // Since the thing storing the board position is located inside an object, I cannot get the information directly from the board file.
 // The input comes in the form of a mulitdimensional array. Only the top face is needed to scan, because the top face is the only place you can place blocks, and lso because its' the only face you can see, 
-std::string get_bot_move(std::array<std::array<std::array<int, 3>, 3>, 6> board_position_input) {
+std::string get_bot_move(std::array<std::array<std::array<int, 3>, 3>, 6> board_position_input, int bot_turn) {
     std::string move;
 
     // Not technically needed, but the original array contains a bunch of redundant variables for this program.
--- src/bot.h
+++ src/bot.h
@@ -1,13 +1,24 @@
 #ifndef BOT_H
 #define BOT_H
 
+
+#include <string>
+#include <iostream>
 #include <array>
 #include <map>
+#include <tuple>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
 #include "board.h"
-
 std::array<std::array<int, 3>, 3> convert_board_position(std::array<std::array<std::array<int, 3>, 3>, 6> unconverted_board_position);
-std::string place_randomly(std::array<std::array<int, 3>, 3> board_position);
-std::string get_bot_move(std::array<std::array<std::array<int, 3>, 3>, 6> board_position_input);
+std::string achieve_win(std::array<std::array<int, 3>, 3> board_position, int bot_turn);
+std::string combat_formed_fork(std::array<std::array<int, 3>, 3> board_position, int bot_turn);
+std::string block_win(std::array<std::array<int, 3>, 3> board_positon, int bot_turn);
+std::string create_fork(std::array<std::array<int, 3>, 3> board_position, int bot_turn);
+std::string block_fork_creation(std::array<std::array<int, 3>, 3> board_position, int bot_turn);
+std::string place_priority(std::array<std::array<int, 3>, 3> board_position, int bot_turn);
+std::string get_bot_move(std::array<std::array<std::array<int, 3>, 3>, 6> board_position_input, int bot_turn);
 
 #endif
