6cb8ef7fc286f59bb946811e96d9f6d9eb521c42
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-18 22:01:05 -0600

    Fixed a bunch of errors

--- src/bot.cpp
+++ src/bot.cpp
@@ -22,11 +22,26 @@ int convert_coords(std::array<int, 2> coords) {
     return converted;
 };
 
-std::string achieve_win(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {};
-std::string combat_formed_fork(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {};
-std::string block_win(std::array<std::array<int, 3>, 3> board_positon, int bot_turn) {};
-std::string create_fork(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {};
-std::string block_fork_creation(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {};
+std::string achieve_win(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {
+    return "nothing";
+};
+
+std::string combat_formed_fork(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {
+    return "nothing";
+};
+
+std::string block_win(std::array<std::array<int, 3>, 3> board_positon, int bot_turn) {
+    return "nothing";
+};
+
+std::string create_fork(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {
+    return "nothing";
+};
+
+std::string block_fork_creation(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {
+    return "nothing";
+};
+
 std::string place_priority(std::array<std::array<int, 3>, 3> board_position, int bot_turn) {
     srand(time(NULL));
     // This function will be split into several parts: center, opposite corner, empty corner, empty side, and rotate board for new face
@@ -37,11 +52,13 @@ std::string place_priority(std::array<std::array<int, 3>, 3> board_position, int
     };
 
     // Opposite corner
-     std::array<std::array<int, 2>, 4> board_corners = {
+    std::array<std::array<int, 2>, 4> board_corners = {{
         {0, 0},
         {0, 2},
         {2, 0},
         {2, 2}
+    }}; // Apparently this is broken, but I have no idea why.
+    
     /*
      * Steps:
      * Scan to board for an occupied corner by the enemy
@@ -55,8 +72,8 @@ std::string place_priority(std::array<std::array<int, 3>, 3> board_position, int
     };
     for (int corners = 0; corners < 4; corners++) {
         std::array<int, 2> current_side = {
-            board_sides[corners][0],
-            board_sides[corners][1]
+            board_corners[corners][0],
+            board_corners[corners][1]
         };
         if (board_position[current_side[0]][current_side[1]] == (3 - bot_turn)) {
             return opposites[corners];
@@ -66,33 +83,33 @@ std::string place_priority(std::array<std::array<int, 3>, 3> board_position, int
     // Empty corner
     for (int corners = 0; corners < 4; corners++) {
         std::array<int, 2> current_side = {
-            board_sides[corners][0],
-            board_sides[corners][1]
+            board_corners[corners][0],
+            board_corners[corners][1]
         };
         if (board_position[current_side[0]][current_side[1]] == 0) {
-            return convert_coords(current_side);
+            return "p" + std::to_string(convert_coords(current_side));
         };
     };
 
     // Empty side
-    std::array<std::array<int, 2>, 4> board_sides = {
+    std::array<std::array<int, 2>, 4> board_sides = {{
         {1, 0},
         {0, 1},
         {2, 1},
         {1, 2}
-    };
+    }};
     for (int sides = 0; sides < 4; sides++) {
         std::array<int, 2> current_side = {
             board_sides[sides][0],
             board_sides[sides][1]
         };
         if (board_position[current_side[0]][current_side[1]] == 0) {
-            return convert_coords(current_side);
+            return "p" + std::to_string(convert_coords(current_side));
         };
     };
 
     // Rotate board
-    rotation_type_number = rand() % 4;
+    int rotation_type_number = rand() % 4;
     switch(rotation_type_number) {
         case 0:
             return "mX";
@@ -105,12 +122,13 @@ std::string place_priority(std::array<std::array<int, 3>, 3> board_position, int
         default:
             return "mD";
     };
+    return "nothing";
 };
 
 // Since the thing storing the board position is located inside an object, I cannot get the information directly from the board file.
 // The input comes in the form of a mulitdimensional array. Only the top face is needed to scan, because the top face is the only place you can place blocks, and lso because its' the only face you can see, 
 std::string get_bot_move(std::array<std::array<std::array<int, 3>, 3>, 6> board_position_input, int bot_turn) {
-    std::string move;
+    std::string move = "mD";
 
     // Not technically needed, but the original array contains a bunch of redundant variables for this program.
     std::array<std::array<int, 3>, 3> converted_board_position = convert_board_position(board_position_input);
@@ -126,4 +144,5 @@ std::string get_bot_move(std::array<std::array<std::array<int, 3>, 3>, 6> board_
     // Empty Corner
     // Empty side
     // Rotate board to new face
+    return move;
 };
