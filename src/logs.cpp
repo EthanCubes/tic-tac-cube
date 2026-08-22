3ab731931d5ea27e7401dc02c57b7c3cfc226f92
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-21 21:46:01 -0600

    Added better logs system

--- src/logs.cpp
+++ src/logs.cpp
@@ -13,9 +13,67 @@ std::string get_time() {
     return current_time;
 };
 
+bool file_exists(std::string filename) {
+    std::ifstream file;
+    file.open(filename);
+    if (!file) {
+        file.close();
+        return false;
+    }
+    else {
+        file.close();
+        return true;
+    };
+};
+
+void create_log_file() {
+    bool name_valid = false;
+    std::string formatted_time = get_time(); // Some of the characters that the file returns are invalid
+
+    for (int i = 0; i < formatted_time.length(); i++) {
+        if (formatted_time[i] == ' ' || formatted_time[i] == ':' || formatted_time[i] == '\n') {
+            formatted_time[i] = '_';
+        };
+    };
+
+    std::string log_name = "build/log_" + formatted_time;
+    std::string log_prefix = ".txt";
+    std::string filename = log_name + log_prefix;
+    while (!name_valid) {
+        if (!file_exists(filename)) {
+            // Move ahead with file creation and logging
+            std::ofstream log_file;
+            log_file.open(filename);
+            log_file << "-------- Beginning of log --------";
+            log_file.close();
+
+            // Create a file to tell the system what the current log file name is
+            log_file.open("build/log_name.txt");
+            log_file << filename;
+            log_file.close();
+
+            name_valid = true;
+        }
+        else {
+            // Unlikely to ever happen, but who knows
+            // Tries again with different name
+            log_name += std::string("-");
+            filename = log_name + log_prefix;
+        }
+    };
+};
+
 void log_data(std::string log_message) {
+    // Get the log file name from file
+    std::ifstream log_file_name_file;
+    log_file_name_file.open("build/log_name.txt");
+    std::string filename;
+    std::getline(log_file_name_file, filename);
+    log_file_name_file.close();
+
+    // Fetch the file and write to the file
     std::string time = get_time();
-    std::ofstream file("game_logs.txt", std::ios::app);
-    file << time << log_message << "\n\n";
-    file.close();
+    std::ofstream log_file(filename, std::ios::app);
+    log_file << time << log_message << "\n\n";
+    log_file.close();
 };
--- src/logs.h
+++ src/logs.h
@@ -5,7 +5,10 @@
 #include <ctime>
 #include <fstream>
 
+
+bool file_exists(std::string filename);
 std::string get_time();
+void create_log_file();
 void log_data(std::string log_message);
 
 #endif
