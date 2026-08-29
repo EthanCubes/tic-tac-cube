5549842dba38914043fd5c231486ca24887bf152
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-29 16:48:27 -0600

    Updated script to not run the program if the compilation fails

--- install.sh
+++ install.sh
@@ -4,5 +4,11 @@ echo "Compiling the program"
 echo "Please be patient"
 # This makes sure to show all the warnings and treasts them as errors
 g++ -std=c++17 -Wall -Wextra -Werror -pedantic -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp -lraylib
-echo "Running the program"
-build/tic-tac-cube
+# AI assisted script
+if [ $? -eq 0 ]; then
+    echo "Running the program"
+    build/tic-tac-cube
+else
+    echo "Compilation failed"
+    exit 1
+fi
