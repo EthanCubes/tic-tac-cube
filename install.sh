0f681b6dfa3305f8bbb2dff62b4f1055027fd7d3
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-03 20:21:27 -0600

    Added new globals.cpp file to the install (compile) script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Compiling the program"
 echo "Please be patient"
 # This makes sure to show all the warnings and treasts them as errors
-g++ -std=c++17 -Wall -Wextra -Werror -pedantic -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp -lraylib
+g++ -std=c++17 -Wall -Wextra -Werror -pedantic -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp -lraylib
 # AI assisted script
 if [ $? -eq 0 ]; then
     echo "Running the program"
