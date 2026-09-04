66256a965c58fb2898d0b2711a471f0fc152ee4e
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-03 21:59:54 -0600

    Added colors to the global files so that all files can use them

--- src/global.cpp
+++ src/global.cpp
@@ -1,2 +1,10 @@
-int screen_width = 1280;
-int screen_height = 720;
+#include "raylib.h"
+
+extern const int screen_width = 1280;
+extern const int screen_height = 720;
+
+extern const Color BACKGROUND_COLOR = {0, 0, 25, 255};
+extern const Color MAIN_MENU_BUTTON_COLOR = {100, 100, 100, 100};
+extern const Color MINOR_BUTTON_COLOR = {100, 100, 100, 100};
+extern const Color TEXT_COLOR_1 = {255, 7, 58, 255};
+extern const Color TEXT_COLOR_2 = {0, 0, 0, 255};
--- src/global.h
+++ src/global.h
@@ -1,7 +1,15 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
-extern int screen_width;
-extern int screen_height;
+#include "raylib.h"
+
+extern const int screen_width;
+extern const int screen_height;
+
+extern const Color BACKGROUND_COLOR;
+extern const Color MAIN_MENU_BUTTON_COLOR;
+extern const Color MINOR_BUTTON_COLOR;
+extern const Color TEXT_COLOR_1;
+extern const Color TEXT_COLOR_2;
 
 #endif
