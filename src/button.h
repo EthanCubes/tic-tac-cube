4e90f7191e7ef9d8efc9e72b9aca530f3cf3550c
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-05 19:57:24 -0600

    Added presets for buttons that will be processed by the button class

--- src/button.h
+++ src/button.h
@@ -16,12 +16,151 @@ using json = nlohmann::json;
  * height
  * color
  * roundness
+ * steps
  * text
  * font size
  * font color
  */
 
-const int UNDEFINED = 0; // This is for the JSON
+const int UNDEFINED = 0; // This is for the JSON. Also, button coors can never be 0, so this does not conflict
+
+// Preset buttons and stuff
+const json TITLE_BUTTON = {
+    {"x_pos", screen_width/2},
+    {"y_pos", screen_height/5},
+    {"width", 0},
+    {"height", 0},
+    {"color", BACKGROUND_COLOR},
+    {"roundness", 0.0f},
+    {"steps", 5},
+    {"text", "Tic-Tac-Cube"},
+    {"font_size", 50},
+    {"font_color", TEXT_COLOR_1}
+};
+
+const json SINGLEPLAYER_BUTTON = {
+    {"x_pos", screen_width/2},
+    {"y_pos", screen_height/20*9},
+    {"width", screen_width/5},
+    {"height", screen_height/10},
+    {"color", MAIN_MENU_BUTTON_COLOR},
+    {"roundness", 0.25f},
+    {"steps", 5},
+    {"text", "Singleplayer"},
+    {"font_size", 40},
+    {"font_color", TEXT_COLOR_1}
+};
+const json MULIPLAYER_BUTTON = {
+    {"x_pos", screen_width/2},
+    {"y_pos", screen_height/20*12},
+    {"width", screen_width/5},
+    {"height", screen_height/10},
+    {"color", MAIN_MENU_BUTTON_COLOR},
+    {"roundness", 0.25f},
+    {"steps", 5},
+    {"text", "Multiplayer"},
+    {"font_size", 40},
+    {"font_color", TEXT_COLOR_1}
+};
+const json HELP_BUTTON = {
+    {"x_pos", screen_width/2},
+    {"y_pos", screen_height/20*15},
+    {"width", screen_width/5},
+    {"height", screen_height/10},
+    {"color", MAIN_MENU_BUTTON_COLOR},
+    {"roundness", 0.25f},
+    {"steps", 5}
+    {"text", "Help"},
+    {"font_size", 40},
+    {"font_color", TEXT_COLOR_1}
+};
+const json EXIT_BUTTON = {
+    {"x_pos", screen_width/2},
+    {"y_pos", screen_height/20*18},
+    {"width", screen_width/5},
+    {"height", screen_height/10},
+    {"color", MAIN_MENU_BUTTON_COLOR},
+    {"roundness", 0.25f},
+    {"steps", 5}
+    {"text", "Exit"},
+    {"font_size", 40},
+    {"font_color", TEXT_COLOR_1}
+};
+
+const json BOARD_BUTTON = {
+    {"x_pos", UNDEFINED},
+    {"y_pos", UNDEFINED},
+    {"width", 100},
+    {"height", 100},
+    {"color", UNDEFINED},
+    {"roundness", 0.25f},
+    {"steps", 5},
+    {"text", UNDEFINED},
+    {"font_size", UNDEFINED},
+    {"font_color", TEXT_COLOR_2}
+};
+const json SQUARE_MOVEMENT_BUTTON = {
+    {"x_pos", UNDEFINED},
+    {"y_pos", UNDEFINED},
+    {"width", 50},
+    {"height", 50},
+    {"color", MINOR_BUTTON_COLOR},
+    {"roundness", 0.25f},
+    {"steps", 5},
+    {"text", UNDEFINED},
+    {"font_size", 20},
+    {"font_color", TEXT_COLOR_1}
+};
+const json RECTANGLE_MOVEMENT_BUTTON_HORIZONTAL = {
+    {"x_pos", UNDEFINED},
+    {"y_pos", UNDEFINED},
+    {"width", 100},
+    {"height", 50},
+    {"color", MINOR_BUTTON_COLOR},
+    {"roundness", 0.25f},
+    {"steps", 5},
+    {"text", UNDEFINED},
+    {"font_size", 20},
+    {"font_color", TEXT_COLOR_1}
+};
+const json RECTANGLE_MOVEMENT_BUTTON_VERTICAL = {
+    {"x_pos", UNDEFINED},
+    {"y_pos", UNDEFINED},
+    {"width", 50},
+    {"height", 100},
+    {"color", MINOR_BUTTON_COLOR},
+    {"roundness", 0.25f},
+    {"steps", 5},
+    {"text", UNDEFINED},
+    {"font_size", 20},
+    {"font_color", TEXT_COLOR_1}
+};
+
+const json POPUP_BUTTON = {
+    {"x_pos", screen_width - 150},
+    {"y_pos", 60},
+    {"width", 300},
+    {"height", 80},
+    {"color", MINOR_BUTTON_COLOR},
+    {"roundness", 0.25f},
+    {"steps", 5},
+    {"text", UNDEFINED},
+    {"font_size", 20},
+    {"font_color", TEXT_COLOR_1}
+};
+
+const json EXIT_GAME_BUTTON = {
+    {"x_pos", 20},
+    {"y_pos", 20},
+    {"width", 40},
+    {"height", 40},
+    {"color", MINOR_BUTTON_COLOR},
+    {"roundness", 0.25f},
+    {"steps", 5},
+    {"text", "<"},
+    {"font_size", 20},
+    {"font_color", TEXT_COLOR_1}
+};
 
 class Button {
     private:
