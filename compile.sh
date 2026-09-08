4fab21b1c5d35263db7f4073f84dc3706c61c043
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-07 18:01:02 -0600

    Added button.cpp to the compiling thing so now I can actually test the code

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@
 echo "Compiling the program"
 echo "Please be patient"
 # This makes sure to show all the warnings. -Wpedantic is the same as -pedantic, and -Wextra-semi adds checking for semicolons
-g++ -std=c++17 -Wall -Wextra -Wextra-semi -Wpedantic -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp -lraylib
+g++ -std=c++17 -Wall -Wextra -Wextra-semi -Wpedantic -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp src/button.cpp -lraylib
