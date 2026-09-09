8610992dbd367bd9876011c9f46f3c40f4500599
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-09 00:03:59 -0600

    Fixed some incorrect file formats

--- src/button.cpp
+++ src/button.cpp
@@ -2,7 +2,11 @@
 #include "raylib.h"
 #include "global.h"
 
+#include "button.h"
+
 using json = nlohmann::json;
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Color, r, g, b, a);
 
 // This file is not yet ready for usage! Don't link it in the install shell script
 
@@ -52,7 +56,7 @@ bool Button::check_hover() {
     Vector2 mouse_pos = GetMousePosition();
     int mouse_x = mouse_pos.x;
     int mouse_y = mouse_pos.y;
-    if (mouse_x > button_info["x_pos"] && mouse_x < button_info["x_pos"]+button_info["width"] && mouse_y > button_info["y_pos"] && mouse_y < button_info["y_pos"]+button_info["height"]) {
+    if (mouse_x > button_info["x_pos"].get<float>() && mouse_x < button_info["x_pos"].get<float>()+button_info["width"].get<float>() && mouse_y > button_info["y_pos"].get<float>() && mouse_y < button_info["y_pos"].get<float>()+button_info["height"].get<float>()) {
         return true;
     };
     return false;
@@ -73,46 +77,46 @@ bool Button::check_button_clicked() {
 void Button::draw_button() {
     // I supposed I could do converting numbers to the correct format on a separate file, but like whatever
     if (check_hover()) {
-        if (button_hover_info["x_pos"] == 0 || button_hover_info["y_pos"] == 0) {
+        if (button_hover_info["x_pos"].get<float>() == 0 || button_hover_info["y_pos"].get<float>() == 0) {
             return;
         };
 
         // Convert coodinates from the center back to the corner for actual drawing. This can actually be 0
-        int calibrated_x = button_hover_info["x_pos"] - button_hover_info["width"]/2;
-        int calibrated_y = button_hover_info["y_pos"] - button_hover_info["height"]/2;
+        int calibrated_x = button_hover_info["x_pos"].get<float>() - button_hover_info["width"].get<float>()/2;
+        int calibrated_y = button_hover_info["y_pos"].get<float>() - button_hover_info["height"].get<float>()/2;
 
         // This part is really hard, we have to center the text by getting the width and then doing some super complex math thing
-        Vector2 text_dimensions = MeasureTextEx(GetFontDefault(), button_hover_info["text"], static_cast<float>(button_hover_info["font_size"]), 3.0f);
+        Vector2 text_dimensions = MeasureTextEx(GetFontDefault(), button_hover_info["text"].get<std::string>(), static_cast<float>(button_hover_info["font_size"].get<float>()), 3.0f);
         int text_width = text_dimensions.x;
         int text_height = text_dimensions.y;
-        int text_x = calibrated_x + (button_hover_info["width"] - text_width)/2;
-        int text_y = calibrated_y + (button_hover_info["height"] - text_height)/2;
+        int text_x = calibrated_x + (button_hover_info["width"].get<float>() - text_width)/2;
+        int text_y = calibrated_y + (button_hover_info["height"].get<float>() - text_height)/2;
         Vector2 text_position = {text_x, text_y};
 
-        Rectangle rectangle_information = {static_cast<float>(button_hover_info["x_pos"]), static_cast<float>(button_hover_info["y_pos"]), static_cast<float>(button_hover_info["width"]), static_cast<float>(button_hover_info["height"])};
-        DrawRectangleRounded(rectangle_information, button_hover_info["roundness"], button_hover_info["steps"], button_hover_info["color"]);
-        DrawTextEx(GetFontDefault(), button_info["text"].c_str(), text_position, static_cast<float>(button_hover_info["font_size"]), 3.0f, button_hover_info["font_color"]);
+        Rectangle rectangle_information = {static_cast<float>(button_hover_info["x_pos"].get<float>()), static_cast<float>(button_hover_info["y_pos"].get<float>()), static_cast<float>(button_hover_info["width"].get<float>()), static_cast<float>(button_hover_info["height"].get<float>())};
+        DrawRectangleRounded(rectangle_information, button_hover_info["roundness"].get<float>(), button_hover_info["steps"].get<float>(), button_hover_info["color"].get<Color>());
+        DrawTextEx(GetFontDefault(), button_info["text"].get<float>.c_str(), text_position, static_cast<float>(button_hover_info["font_size"].get<float>()), 3.0f, button_hover_info["font_color"].get<Color>());
     }
     else {
         // Not on hover
-        if (button_info["x_pos"] == 0 || button_info["y_pos"] == 0) {
+        if (button_info["x_pos"].get<float>() == 0 || button_info["y_pos"].get<float>() == 0) {
             return;
         };
 
         // Convert coodinates from the center back to the corner for actual drawing. This can actually be 0
-        int calibrated_x = button_info["x_pos"] - button_info["width"]/2;
-        int calibrated_y = button_info["y_pos"] - button_info["height"]/2;
+        int calibrated_x = button_info["x_pos"].get<float>() - button_info["width"].get<float>()/2;
+        int calibrated_y = button_info["y_pos"].get<float>() - button_info["height"].get<float>()/2;
 
         // This part is really hard, we have to center the text by getting the width and then doing some super complex math thing
-        Vector2 text_dimensions = MeasureTextEx(GetFontDefault(), button_info["text"], static_cast<float>(button_info["font_size"]), 3.0f);
+        Vector2 text_dimensions = MeasureTextEx(GetFontDefault(), button_info["text"].get<std::string>(), static_cast<float>(button_info["font_size"].get<float>()), 3.0f);
         int text_width = text_dimensions.x;
         int text_height = text_dimensions.y;
-        int text_x = calibrated_x + (button_info["width"] - text_width)/2;
-        int text_y = calibrated_y + (button_info["height"] - text_height)/2;
-        Vector2 text_position = {text_x, text_y};
+        int text_x = calibrated_x + (button_info["width"].get<float>() - text_width)/2;
+        int text_y = calibrated_y + (button_info["height"].get<float>() - text_height)/2;
+        Vector2 text_position = {static_cast<float>(text_x), static_cast<float>(text_y)};
 
-        Rectangle rectangle_information = {static_cast<float>(calibrated_x), static_cast<float>(calibrated_y), static_cast<float>(button_info["width"]), static_cast<float>(button_info["height"])};
-        DrawRectangleRounded(rectangle_information, button_info["roundness"], button_info["steps"], button_info["color"]);
-        DrawTextEx(GetFontDefault(), button_info["text"].c_str(), text_position, static_cast<float>(button_info["font_size"]), 3.0f, button_info["font_color"]);
+        Rectangle rectangle_information = {static_cast<float>(calibrated_x), static_cast<float>(calibrated_y), static_cast<float>(button_info["width"].get<float>()), static_cast<float>(button_info["height"].get<float>())};
+        DrawRectangleRounded(rectangle_information, button_info["roundness"].get<float>(), button_info["steps"].get<float>(), button_info["color"].get<Color>());
+        DrawTextEx(GetFontDefault(), button_info["text"].get<std::string>().c_str(), text_position, static_cast<float>(button_info["font_size"].get<float>()), 3.0f, button_info["font_color"].get<Color>());
     };
 };
