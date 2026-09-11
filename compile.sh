c866e4f5eac2d3638ca71a43a514f6f5dc3f3c22
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-11 15:02:49 -0600

    Made install script easier to read, added display of time

--- compile.sh
+++ compile.sh
@@ -3,6 +3,12 @@
 echo "Compiling the program"
 echo "Please be patient"
 # This makes sure to show all the warnings. -Wpedantic is the same as -pedantic, and -Wextra-semi adds checking for semicolons
-g++ -std=c++17 -Wall -Wextra -Wextra-semi -Wpedantic -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp src/button.cpp -lraylib
+
+# Normal
+#g++ -std=c++17 -Wall -Wextra -Wextra-semi -Wpedantic -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp src/button.cpp -lraylib
+
+# Display time
+g++ -ftime-report -std=c++17 -Wall -Wextra -Wextra-semi -Wpedantic -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp src/button.cpp -lraylib
+
+# No warnings
 #g++ -std=c++17 -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp src/button.cpp -lraylib
-#g++ -std=c++17 -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp src/button.cpp -lraylib 2>&1 | grep -E error | head -20
