c2e9085b5f27ad760ceba5cd76e297b53c639179
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-13 00:31:13 -0600

    Changed back to not having a time display since I didn't read it anyway

--- compile.sh
+++ compile.sh
@@ -5,10 +5,10 @@ echo "Please be patient"
 # This makes sure to show all the warnings. -Wpedantic is the same as -pedantic, and -Wextra-semi adds checking for semicolons
 
 # Normal
-#g++ -std=c++17 -Wall -Wextra -Wextra-semi -Wpedantic -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp src/button.cpp -lraylib
+g++ -std=c++17 -Wall -Wextra -Wextra-semi -Wpedantic -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp src/button.cpp -lraylib
 
 # Display time
-g++ -ftime-report -std=c++17 -Wall -Wextra -Wextra-semi -Wpedantic -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp src/button.cpp -lraylib
+#g++ -ftime-report -std=c++17 -Wall -Wextra -Wextra-semi -Wpedantic -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp src/button.cpp -lraylib
 
 # No warnings
 #g++ -std=c++17 -o build/tic-tac-cube src/main.cpp src/board.cpp src/bot.cpp src/logs.cpp src/global.cpp src/button.cpp -lraylib
