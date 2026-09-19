94bf7fa13cf267fa36260de9d3cf01a5d731fe8d
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-18 22:11:20 -0600

    Added global variables for large and small font

--- src/global.cpp
+++ src/global.cpp
@@ -14,6 +14,8 @@ extern const Color TEXT_COLOR_2 = {0, 0, 0, 255};
 
 extern const Color BUTTON_HOVER_COLOR = {68, 82, 187, 255};
 
+extern Font FONT_SMALL;
 extern Font FONT;
+extern Font FONT_BIG;
 
 bool font_setup = false;
